19d7320382f8c8750ac66c835acdc290cbac2e16
Hope4All Clinic <info@hope4allclinic.co.za>
2026-09-18 09:42:14 +0200

    Revert drip image from service card

--- js/app.js
+++ js/app.js
@@ -203,7 +203,6 @@ function renderServiceCards() {
       icon: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 12h-4l-3 9L9 3l-3 9H2"/></svg>`,
       title: "Drip Therapy",
       desc: "Professional intravenous therapy administered by qualified healthcare practitioners.",
-      image: "drip.png",
       items: [],
     },
     {
@@ -242,7 +241,6 @@ function renderServiceCards() {
     }
 
     card.innerHTML = `
-      ${service.image ? `<img class="service-card-img" src="${service.image}" alt="${service.title}" loading="lazy">` : ""}
       <div class="service-icon">${service.icon}</div>
       <h3>${service.title}</h3>
       <p>${service.desc}</p>
